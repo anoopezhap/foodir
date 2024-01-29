ff713fd04cdd335fcb90077a68031367e4574c82
anoopezhap <anoopezhap@gmail.com>
2024-01-29 16:44:56 -0500

    jan 29 work

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>MEAl NOT FOUND</h1>
+      <p>Unfortunately the requested meal cannot be found</p>
+    </main>
+  );
+}
